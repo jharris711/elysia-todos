7640bd8acfa088961c8b948db05f22ba2dde4a51
Josh Harris <js.harris711@gmail.com>
2026-02-25 03:39:12 -0500

    Initial Elysia app, full-stack dev server setup, tailwind/react

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import { Elysia } from "elysia";
+import { staticPlugin } from '@elysiajs/static';
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+
+const app = new Elysia()
+  .use(await staticPlugin({
+    prefix: '/'
+  }))
+;
+  
+app.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
